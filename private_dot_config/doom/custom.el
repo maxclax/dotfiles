c7ba0093b1951d931d76307700fb0c3a17ddb71f
dev <dev>
2025-02-15 16:37:06 +0100

    Updatings

--- private_dot_config/doom/custom.el
+++ private_dot_config/doom/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(copilot-chat copilot file-info)))
+ '(package-selected-packages '(org-roam-ui org-roam command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
