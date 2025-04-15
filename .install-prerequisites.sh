267f2f4ca4102ba58b2508a439604f9c39ae18ff
dev <dev>
2025-04-15 17:23:57 +0200

    fix: install pre

--- .install-prerequisites.sh
+++ .install-prerequisites.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+install_brew() {
+	if which brew >/dev/null 2>&1; then
+		echo 'Homebrew is already installed'
+	else
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+}
+
 install_pkgx() {
 	if which pkgx >/dev/null 2>&1; then
 		echo 'pkgx is already installed'
@@ -8,35 +16,7 @@ install_pkgx() {
 	fi
 }
 
-install_1password() {
-	if which op >/dev/null 2>&1; then
-		echo '1Password CLI is already installed'
-	else
-		echo 'Installing 1Password CLI...'
-		# For macOS
-		if [ "$OS" = "Darwin" ]; then
-			brew install --cask 1password 1password-cli
-		# For Linux
-		# elif [ "$OS" = "Linux" ]; then
-		# 	sudo apt update && sudo apt install 1password 1password-cli
-		fi
-	fi
-}
-
-install_age() {
-	if which age >/dev/null 2>&1; then
-		echo 'age is already installed'
-	else
-		echo 'Installing age...'
-		# For macOS
-		if [ "$OS" = "Darwin" ]; then
-			brew install age
-		# For Linux
-		elif [ "$OS" = "Linux" ]; then
-			sudo apt update && sudo apt install -y age
-		fi
-	fi
-
+generate_key() {
 	# Only generate key if age is installed and key doesn't exist
 	if which age >/dev/null 2>&1 && which age-keygen >/dev/null 2>&1; then
 		if [ ! -f ~/.config/chezmoi/key.txt ]; then
@@ -53,10 +33,13 @@ install_age() {
 
 install_on_linux() {
 	echo "Installing prerequisites for Linux..."
-	sudo apt update && sudo apt install -y curl git wget
+	sudo apt update && sudo apt install -y curl git wget age
+
+	# pkgx
 	install_pkgx
-	install_1password
-	install_age
+
+	# Generate key
+	generate_key
 }
 
 install_on_mac() {
@@ -86,17 +69,17 @@ install_on_mac() {
 	install_brew
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 
+	# pkgx
 	install_pkgx
-	install_1password
-	install_age
-}
 
-install_brew() {
-	if which brew >/dev/null 2>&1; then
-		echo 'Homebrew is already installed'
-	else
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	fi
+	echo "Installing prerequisites for macOS..."
+	brew install age
+
+	# Install 1Password CLI and GUI
+	brew install --cask 1password 1password-cli
+
+	# Generate key
+	generate_key
 }
 
 OS="$(uname -s)"
