4a2465ece2fe40a91aa4fc48ab98a440fc591001
dev <dev>
2025-05-06 15:13:22 +0200

    chore(nvim): updating

--- private_dot_config/nvim-custom/lua/plugins/core/blink.lua
+++ private_dot_config/nvim-custom/lua/plugins/core/blink.lua
@@ -1,83 +1,83 @@
 return {
 
-  {
-    "saghen/blink.cmp",
-    lazy = false, -- lazy loading handled internally
-    version = "*",
-    dependencies = {
-      -- NOTE: https://github.com/Saghen/blink.compat is also available
-      "rafamadriz/friendly-snippets",
-    },
+	{
+		"saghen/blink.cmp",
+		lazy = false, -- lazy loading handled internally
+		version = "*",
+		dependencies = {
+			-- NOTE: https://github.com/Saghen/blink.compat is also available
+			"rafamadriz/friendly-snippets",
+		},
 
-    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    --  build = "cargo build --release",
+		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		--  build = "cargo build --release",
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = require("config.keymaps").setup_blink_cmp_keymaps(),
-      cmdline = {
-        enabled = true,
-        completion = {
-          menu = { auto_show = true },
-          ghost_text = { enabled = true },
-        },
-        keymap = require("config.keymaps").setup_blink_cmdline_keymaps(),
-        -- keymap = { preset = "cmdline" },
-      },
-      completion = {
-        list = {
-          selection = {
-            preselect = false,
-            auto_insert = false,
-          },
-        },
-        documentation = {
-          auto_show = true,
-        },
-        menu = {
-          draw = {
-            treesitter = { "lsp" },
-          },
-        },
-      },
-      signature = {
-        enabled = true, -- experimental, can also be provided by noice
-      },
-      appearance = {
-        kind_icons = require("utils.icons").icons.kinds,
-      },
-      -- default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, via `opts_extend`
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-        providers = {
-          path = {
-            -- TODO: use custom field and move to respective plugin
-            enabled = function()
-              return not vim.tbl_contains({ "AvanteInput", "codecompanion" }, vim.bo.filetype)
-            end,
-          },
-          -- TODO: use custom field and move to respective plugin
-          buffer = {
-            enabled = function()
-              return not vim.tbl_contains({ "AvanteInput", "codecompanion" }, vim.bo.filetype)
-            end,
-          },
-          snippets = {
-            opts = {
-              friendly_snippets = true,
-              search_paths = { require("utils.environ").getenv("DOTFILES") .. "/nvim-snippets" },
-            },
-          },
-        },
-      },
-    },
-    opts_extend = {
-      "sources.default",
-    },
-    config = function(_, opts)
-      require("blink.cmp").setup(opts)
-    end,
-  },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = require("config.keymaps").setup_blink_cmp_keymaps(),
+			cmdline = {
+				enabled = true,
+				completion = {
+					menu = { auto_show = true },
+					ghost_text = { enabled = true },
+				},
+				keymap = require("config.keymaps").setup_blink_cmdline_keymaps(),
+				-- keymap = { preset = "cmdline" },
+			},
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+						auto_insert = false,
+					},
+				},
+				documentation = {
+					auto_show = true,
+				},
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+					},
+				},
+			},
+			signature = {
+				enabled = true, -- experimental, can also be provided by noice
+			},
+			appearance = {
+				kind_icons = require("utils.icons").icons.kinds,
+			},
+			-- default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, via `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				providers = {
+					path = {
+						-- TODO: use custom field and move to respective plugin
+						enabled = function()
+							return not vim.tbl_contains({ "AvanteInput", "codecompanion" }, vim.bo.filetype)
+						end,
+					},
+					-- TODO: use custom field and move to respective plugin
+					buffer = {
+						enabled = function()
+							return not vim.tbl_contains({ "AvanteInput", "codecompanion" }, vim.bo.filetype)
+						end,
+					},
+					snippets = {
+						opts = {
+							friendly_snippets = true,
+							search_paths = { vim.fn.expand("~/.config/nvim-custom/snippets") },
+						},
+					},
+				},
+			},
+		},
+		opts_extend = {
+			"sources.default",
+		},
+		config = function(_, opts)
+			require("blink.cmp").setup(opts)
+		end,
+	},
 }
--- private_dot_config/nvim-custom/lua/plugins/winshift.lua
+++ private_dot_config/nvim-custom/lua/plugins/winshift.lua
@@ -1,8 +1,8 @@
 return {
-	{
-		"sindrets/winshift.nvim",
-		lazy = true,
-		keys = require("config.keymaps").setup_winshift_keymaps(),
-		cmd = { "WinShift" },
-	},
+  {
+    "sindrets/winshift.nvim",
+    lazy = true,
+    keys = require("config.keymaps").setup_winshift_keymaps(),
+    cmd = { "WinShift" },
+  },
 }
