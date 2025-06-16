9c0d88b849ebc0a6016d05ffd6727e7c2db11920
dev <dev>
2025-06-16 18:38:48 +0200

    feat(shell): helpers

--- private_dot_config/shell/sourcing.sh
+++ private_dot_config/shell/sourcing.sh
@@ -114,6 +114,43 @@ else
 	####
 fi
 
+# ----------------------------
+# custom helpers
+# ----------------------------
+
+function j() {
+	cd "$(find . -type d 2>/dev/null | fzf)" || return
+}
+
+function killport() {
+	lsof -ti tcp:$1 | xargs kill -9
+}
+
+function extract() {
+	if [ -f "$1" ]; then
+		case "$1" in
+		*.tar.bz2) tar xjf "$1" ;;
+		*.tar.gz) tar xzf "$1" ;;
+		*.bz2) bunzip2 "$1" ;;
+		*.rar) unrar x "$1" ;;
+		*.gz) gunzip "$1" ;;
+		*.tar) tar xf "$1" ;;
+		*.tbz2) tar xjf "$1" ;;
+		*.tgz) tar xzf "$1" ;;
+		*.zip) unzip "$1" ;;
+		*.Z) uncompress "$1" ;;
+		*.7z) 7z x "$1" ;;
+		*) echo "'$1' cannot be extracted via extract()" ;;
+		esac
+	else
+		echo "'$1' is not a valid file"
+	fi
+}
+
+function hstart() {
+	history | grep -E "^ *[0-9]+ +$1"
+}
+
 # ----------------------------------
 # overrides
 # ----------------------------------
