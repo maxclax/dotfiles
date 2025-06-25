bf47b5ffe0fcb815e83d88dfa869eab6cd127b2a
dev <dev>
2025-06-25 10:32:24 +0200

    fix: volar in nvim

--- private_dot_config/nvim-custom/lua/plugins/lang/vue.lua
+++ private_dot_config/nvim-custom/lua/plugins/lang/vue.lua
@@ -14,8 +14,36 @@ return {
 						"mason-org/mason.nvim",
 					},
 				},
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					vim.list_extend(opts.ensure_installed, { "vue_ls" })
+				end,
 			},
 		},
-		opts = {},
+		opts = {
+			servers = {
+				---@type vim.lsp.Config
+				vue_ls = {
+					cmd = { "vue-language-server", "--stdio" },
+					filetypes = { "vue" },
+					root_markers = { "package.json", "vue.config.js", "vite.config.ts", "vite.config.js", ".git" },
+					init_options = {
+						typescript = {
+							tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+						},
+					},
+					settings = {
+						vue = {
+							completeFunctionCalls = true,
+							updateImportsOnFileMove = { enabled = "always" },
+							inlayHints = {
+								missingProps = true,
+								optionsWrapper = true,
+							},
+						},
+					},
+				},
+			},
+		},
 	},
 }
