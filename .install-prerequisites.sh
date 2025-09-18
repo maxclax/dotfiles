16460d3cee27b41db8e044e1fecd07f3ae0e6cb7
dev <dev>
2025-09-18 10:44:43 +0200

    fix: support user nix profile sourcing fallback

--- .install-prerequisites.sh
+++ .install-prerequisites.sh
@@ -36,7 +36,11 @@ install_nix() {
 			curl -L https://nixos.org/nix/install | sh
 		fi
 		
-		source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+		if [ -f '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+			source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+		elif [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+			source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+		fi
 	fi
 }
 
