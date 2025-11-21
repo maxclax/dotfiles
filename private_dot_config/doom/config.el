ad2dabccccc9c772d63ccee6a70d5749df66a523
dev <dev>
2025-11-21 14:01:31 +0100

    dev: reset doom and added forked purcell

--- private_dot_config/doom/config.el
+++ private_dot_config/doom/config.el
@@ -1,72 +1,76 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Load all configuration modules
-(load! "+os")
-(load! "lisp/init-git.el")
-(load! "lisp/init-org.el")
-(load! "lisp/init-pomodoro.el")
-(load! "lisp/init-bibliography.el")
-(load! "lisp/init-ai.el")
-(load! "+misc")
-(load! "+text")
-(load! "+prog")
-(load! "+ui")
-(load! "+keys")
-(load! "lisp/init-denote")
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-;; Autoload all files from the modules directory
-(let ((modules-dir (expand-file-name "helpers" doom-user-dir)))
-  (when (file-directory-p modules-dir)
-    (dolist (file (directory-files modules-dir t "\\.el$"))
-      (load file nil 'nomessage))))
 
-;; Conditional LSP configuration
-(cond
- ((modulep! :tools lsp +eglot) (load! "lisp/init-eglot.el"))
- ((modulep! :tools lsp) (load! "lisp/init-lsp.el")))
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
-(setq bookmark-default-file "~/.config/doom/bookmarks")
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-(setq user-full-name "dev"
-      user-mail-address "dev@site.com")
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
 
-(setq doom-scratch-buffer-major-mode 'emacs-lisp-mode
-      confirm-kill-emacs nil)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
-(setq-default fill-column 120
-              delete-trailing-lines t)
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
-;; Delete the selection when pasting
-(delete-selection-mode 1)
 
-;; Remap Cmd+C and Cmd+V to copy/paste in GUI Emacs when evil mode is active
-;; Note: s- is remapped to M- in +keys.el, so we use M- here
-(when (and (display-graphic-p) (featurep 'evil))
-  (global-set-key (kbd "M-c") 'kill-ring-save) ; Cmd+C (remapped to Meta)
-  (global-set-key (kbd "M-v") 'yank))          ; Cmd+V (remapped to Meta)
-
-;; disable risky local variables warning
-(advice-add 'risky-local-variable-p :override #'ignore)
-
-(add-hook! 'find-file-hook #'+my/find-file-check-make-large-file-read-only-hook)
-
-;; Global Auto-Revert Mode is a global minor mode that reverts any
-;; buffer associated with a file when the file changes on disk
-(global-auto-revert-mode)
-
-;; check minified-file
-(add-to-list 'magic-mode-alist (cons #'+my/check-minified-file 'fundamental-mode))
-
-;; Manually edit .local/custom.el will break doom updates
-(when (file-directory-p custom-file)
-  (message (concat "Please delete " custom-file ". And customization in config.el and +ui.el.")))
-
-(custom-set-variables
- '(warning-suppress-log-types '((lsp-mode) (iedit)))
- '(warning-suppress-types '((iedit))))
-
-;; Load system profile for different machines and work config
-(dolist (config '("~/.config/doom/local.el"))
-  (let ((config-file (file-truename config)))
-    (when (file-exists-p config-file)
-      (load-file config-file))))
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
--- private_dot_config/doom/modules/private/my-python/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; private/my-python/packages.el
-
-;; (package! importmagic)
-(package! uv-mode)
-(package! py-isort)
